287904c61da592575dfe08683bd038392aaf39cf
Amelia Alfonso <alfonsoa@bu.edu>
2024-09-29 20:14:47 -0400

    Added assignment 2

--- components/assignments.js
+++ components/assignments.js
@@ -8,16 +8,4 @@ const assignments = () => (
       <p>Brief explanation of how you approached the assignment.</p>
       <a href="https://github.com/username/username-assignment-1">Github Link</a>
       <h3>Assignment 2: K Means Cluser Visualizer</h3>
-      <p>This assignment involved creating an interactive web application that 
-        demonstrates the KMeans clustering algorithm with various initialization methods, 
-        allowing users to visualize the clustering process step-by-step. The project requires 
-        functionality to generate datasets, select initialization strategies, and dynamically 
-        update cluster assignments on a 2D plot, with the entire setup tested via a Makefile and GitHub Actions.
-    </p>
-      <a href="https://github.com/ame0101/alfonsoa-assignment-2">Github Link</a>
-    </div>
-    {/* Add more assignments here */}
-  </section>
-);
-
-export default assignments;
+      <p>This assignment involved crea
